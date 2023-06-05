50f051200916b37a04bf910e1e33c302484d1ade
Nurullah KANBAK <nurullahkanbak@Nurullahs-MacBook-Pro.local>
2023-06-05 13:07:02 +0300

    social login is implemented

---
+++ app/types/index.ts
@@ -0,0 +1,10 @@
+import { User } from "@prisma/client";
+
+export type SafeUser = Omit<
+	User,
+	"createdAt" | "updatedAt" | "emailVerified"
+> & {
+	createdAt: string;
+	updatedAt: string;
+	emailVerified: string | null;
+};
